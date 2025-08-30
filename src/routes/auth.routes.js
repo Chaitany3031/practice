ae01f025f3fd9d672a51422acbfb978e9e41ba74
Chaitany3031 <chaitanyamhatre2349@gmail.com>
2025-08-30 21:43:11 +0530

    all 4 apis done

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const userModel = require('../models/user.model.js')
 const jwt = require('jsonwebtoken')
+const { is } = require('express/lib/request.js')
 
 const router = express.Router()
 
@@ -29,6 +30,71 @@ router.post('/register',async (req,res)=>{
         user
     })
 
+})
+
+router.get('/user',async (req,res)=>{
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message:"unauthorized token not found"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+        const user = await userModel.findOne({
+            _id:decoded.id
+        })
+
+        return res.status(200).json({
+            message:"user fetched successfully",
+            user
+        })
+
+    } catch (error) {
+          res.status(401).json({
+            message:"unauthorized"
+        })
+    }
+
+})
+
+router.post('/login',async (req,res)=>{
+    const {username,password} = req.body
+
+    const user = await userModel.findOne({username})
+
+    if(!user){
+        return res.status(404).json({
+            message:"user not found"
+        })
+    }
+
+    const isValidPassword = user.password === password 
+
+    if(!isValidPassword){
+        return res.status(401).json({
+            message:"invalid password"
+        })
+    }
+
+    const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+
+    res.cookie("token",token)
+
+    res.status(200).json({
+        message:"user login successful"
+    })
+})
+
+router.get('/logout',(req,res)=>{
+    res.clearCookie("token")
+
+    res.status(200).json({
+        message:"user logged out"
+    })
 })
 
 module.exports = router
